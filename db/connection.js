bf171b4d9a24f79ca837bf15f856fafe41d7fd27
soumik-saha-38 <soumik38saha99@gmail.com>
2023-12-23 22:50:24 +0530

    2nd commit

---
+++ db/connection.js
@@ -0,0 +1,5 @@
+const mongoose=require('mongoose')
+require('dotenv').config()
+mongoose.connect(process.env.REACT_APP_MONGO_URL,{useNewUrlParser:true,useUnifiedTopology:true})
+.then(()=>console.log('db connected'))
+.catch((e)=>console.log('error',e))
---
+++ index.js
@@ -0,0 +1,96 @@
+const {Server}=require('socket.io')
+const express = require('express')
+const app = express()
+const cors=require('cors')
+app.use(express.json())
+app.use(express.urlencoded({extended:false}))
+const server=require('http').createServer(app)
+require('./db/connection')
+const Users=require('./models/Users')
+
+const io=new Server(5000,{  //socket io server
+    cors:true
+})
+
+app.use(cors())
+
+const emailToSocket=new Map()
+const socketToEmail=new Map()
+
+io.on('connection', (socket) => {
+    console.log(`${socket.id} is connected`)
+	socket.emit('me', socket.id);
+
+    socket.on('join_room',(data)=>{
+        const {roomNo,email}=data
+        emailToSocket.set(email,socket.id)
+        socketToEmail.set(socket.id,email)
+        io.to(roomNo).emit('user_joined',{email,id:socket.id}) //alert everyone in the room that a user has joined
+        socket.join(roomNo)
+        io.to(socket.id).emit('join_room',data)
+    })
+
+	socket.on('call_user', ({sendTo,offer}) => {
+		io.to(sendTo).emit('call_incoming', {receivedFrom:socket.id, offer})
+	})
+
+	socket.on('call_accepted',({sendTo,ans}) => {
+		io.to(sendTo).emit('call_accepted', {receivedFrom:socket.id, ans})
+	})
+
+    socket.on('negotiation_needed',({sendTo,offer})=>{
+        io.to(sendTo).emit('negotiation_needed', {receivedFrom:socket.id,offer})
+    })
+
+    socket.on('negotiation_done', ({sendTo, ans }) => {
+        // console.log('neg done',ans)
+        io.to(sendTo).emit('negotiation_complete', {receivedFrom: socket.id, ans })
+    })
+})
+
+app.get('/',cors(),(req,res)=>{})
+
+app.post('/signup',async (req,res)=>{
+    // console.log(res)
+    const{name,email,pass}=req.body
+    try{
+        const check=await Users.findOne({email:email})
+        if(check){
+            res.json('exists')
+        }else{
+            res.json('notexists')
+            const data={
+                name:name,
+                email:email,
+                pass:pass
+            }
+            await Users.insertMany([data])
+        }
+    }catch(e){
+        console.log(e)
+        res.json('notexists')
+    }
+})
+
+app.post('/signin',async (req,res)=>{
+    const{email,pass}=req.body
+    try{
+        const check=await Users.findOne({email:email})
+        if(check){
+            if(check.pass===pass){
+                res.json('authorize')
+            }else{
+                res.json('wrongpass')
+            }
+        }else{
+            res.json('notexists')
+        }
+    }catch(e){
+        res.json('notexists')
+    }
+})
+
+const port = process.env.PORT || 4000
+server.listen(port, () => {
+    console.log(`Server running on ${port}`)
+})
